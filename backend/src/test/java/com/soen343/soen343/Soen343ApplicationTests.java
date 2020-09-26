1e6f0ba07d82cb19a81917746f8acbc432ea209e
Josh Fried <josh.fried13@gmail.com>
2020-09-26 08:59:13 -0400

    Initial commit for backend

---
+++ backend/src/test/java/com/soen343/soen343/Soen343ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen343.soen343;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soen343ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
