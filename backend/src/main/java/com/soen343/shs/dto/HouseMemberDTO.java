9a7b76145f8729b7754cf4f29f201fba39d528da
Simon Lim <simonlim898@hotmail.com>
2020-10-09 00:12:55 -0400

    #23 Added DTO for HouseLayout

--- backend/src/main/java/com/soen343/shs/dto/HouseMemberDTO.java
+++ backend/src/main/java/com/soen343/shs/dto/HouseMemberDTO.java
@@ -1,2 +1,15 @@
-package com.soen343.shs.dto;public class HouseMemberDTO {
+package com.soen343.shs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class HouseMemberDTO {
+    private String name;
+    private long roomId;
 }
--- backend/src/main/java/com/soen343/shs/dto/RoomLayoutDTO.java
+++ backend/src/main/java/com/soen343/shs/dto/RoomLayoutDTO.java
@@ -1,2 +1,15 @@
-package com.soen343.shs.dto;public class RoomLayoutDTO {
+package com.soen343.shs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class RoomLayoutDTO {
+    private int nbOfWindows;
+    private int nbOfLights;
 }
