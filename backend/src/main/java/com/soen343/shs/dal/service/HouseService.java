2bfe23ecd7feb3154c21cf3445024cad0f6db4af
mattfrances <matfran23@gmail.com>
2020-10-16 12:00:59 -0400

    cleaned up code and return dto's

--- backend/src/main/java/com/soen343/shs/dal/service/HouseService.java
+++ backend/src/main/java/com/soen343/shs/dal/service/HouseService.java
@@ -10,13 +10,16 @@ import com.soen343.shs.dal.repository.LightRepository;
 import com.soen343.shs.dal.service.exceptions.exteriorDoor.ExteriorDoorAlreadyLockedException;
 import com.soen343.shs.dal.service.exceptions.exteriorDoor.ExteriorDoorAlreadyUnlockedException;
 import com.soen343.shs.dal.service.exceptions.exteriorDoor.ExteriorDoorNotFoundException;
+import com.soen343.shs.dal.service.exceptions.house.HouseNotFoundException;
 import com.soen343.shs.dal.service.exceptions.houseWindow.*;
 import com.soen343.shs.dal.service.exceptions.light.LightIsAlreadyOffException;
 import com.soen343.shs.dal.service.exceptions.light.LightIsAlreadyOnException;
 import com.soen343.shs.dal.service.exceptions.light.LightNotFoundException;
+import com.soen343.shs.dto.DoorDTO;
+import com.soen343.shs.dto.LightDTO;
+import com.soen343.shs.dto.WindowDTO;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class HouseService {
@@ -24,24 +27,39 @@ public class HouseService {
     private final HouseWindowRepository houseWindowRepository;
     private final InteriorDoorRepository interiorDoorRepository;
     private final ExteriorDoorRepository exteriorDoorRepository;
+    private final ConversionService mvcConversionService;
 
-    public HouseService(final LightRepository lightRepository, final HouseWindowRepository houseWindowRepository, final InteriorDoorRepository interiorDoorRepository, final ExteriorDoorRepository exteriorDoorRepository) {
+    public HouseService(final LightRepository lightRepository,
+                        final HouseWindowRepository houseWindowRepository,
+                        final InteriorDoorRepository interiorDoorRepository,
+                        final ExteriorDoorRepository exteriorDoorRepository,
+                        final ConversionService mvcConversionService) {
         this.lightRepository = lightRepository;
         this.houseWindowRepository = houseWindowRepository;
         this.interiorDoorRepository = interiorDoorRepository;
         this.exteriorDoorRepository = exteriorDoorRepository;
+        this.mvcConversionService = mvcConversionService;
     }
 
-    public void openWindow(long windowId) {
+    private String getWindowNotFoundError(long windowId) {
+        return "Error: Window with ID " + windowId + " does not exist. Please enter a valid window ID.";
+    }
+
+    private String getHouseNotFoundError(long houseId) {
+        return "Error: House with ID " + houseId + " does not exist. Please enter a valid house ID.";
+    }
+
+    private String getExteriorDoorNotFoundError(long exteriorDoorId) {
+        return "Error: Exterior door with ID " + exteriorDoorId + " does not exist. Please enter a valid exterior door ID.";
+    }
+
+    private String getLightNotFoundError(long lightId) {
+        return "Error: Light with ID " + lightId + " does not exist. Please enter a valid light ID.";
+    }
+
+    public WindowDTO openWindow(long windowId) {
         // Get the window from the db
-        final Optional<HouseWindow> optionalWindow = houseWindowRepository.findById(windowId);
-
-        // Check if window exists
-        if (!optionalWindow.isPresent()) {
-            throw new HouseWindowNotFoundException("Error: Window with ID " + windowId + " does not exist. Please enter a valid window ID.");
-        }
-
-        HouseWindow window = optionalWindow.get();
+        final HouseWindow window = houseWindowRepository.findById(windowId).orElseThrow(() -> new HouseNotFoundException(getHouseNotFoundError(windowId)));
 
         // Check if window is already opened
         if (window.isOpen()) {
@@ -56,18 +74,14 @@ public class HouseService {
         // Open the window
         window.setOpen(true);
         houseWindowRepository.save(window);
+
+        // Return HouseWindowDTO to client
+        return mvcConversionService.convert(window, WindowDTO.class);
     }
 
-    public void closeWindow(long windowId) {
+    public WindowDTO closeWindow(long windowId) {
         // Get the window from the db
-        final Optional<HouseWindow> optionalWindow = houseWindowRepository.findById(windowId);
-
-        // Check if window exists
-        if (!optionalWindow.isPresent()) {
-            throw new HouseWindowNotFoundException("Error: Window with ID " + windowId + " does not exist. Please enter a valid window ID.");
-        }
-
-        HouseWindow window = optionalWindow.get();
+        final HouseWindow window = houseWindowRepository.findById(windowId).orElseThrow(() -> new HouseNotFoundException(getHouseNotFoundError(windowId)));
 
         // Check if window is already opened
         if (!window.isOpen()) {
@@ -82,18 +96,14 @@ public class HouseService {
         // Close the window
         window.setOpen(false);
         houseWindowRepository.save(window);
+
+        // Return HouseWindowDTO to client
+        return mvcConversionService.convert(window, WindowDTO.class);
     }
 
-    public void unlockExteriorDoor(long exteriorDoorId) {
+    public DoorDTO unlockExteriorDoor(long exteriorDoorId) {
         // Get the door from the db
-        final Optional<ExteriorDoor>  optionalExteriorDoor = exteriorDoorRepository.findById(exteriorDoorId);
-
-        // Check if door exists
-        if (!optionalExteriorDoor.isPresent()) {
-            throw new ExteriorDoorNotFoundException("Error: Exterior door with ID " + exteriorDoorId + " not found. Please enter a valid exterior door ID.");
-        }
-
-        ExteriorDoor exteriorDoor = optionalExteriorDoor.get();
+        final ExteriorDoor exteriorDoor = exteriorDoorRepository.findById(exteriorDoorId).orElseThrow(() -> new ExteriorDoorNotFoundException(getExteriorDoorNotFoundError(exteriorDoorId)));
 
         // Check if door is already unlocked
         if (!exteriorDoor.isLocked()) {
@@ -103,18 +113,14 @@ public class HouseService {
         // Unlock door
         exteriorDoor.setLocked(false);
         exteriorDoorRepository.save(exteriorDoor);
+
+        // Return DoorDTO to client
+        return mvcConversionService.convert(exteriorDoor, DoorDTO.class);
     }
 
-    public void lockExteriorDoor(long exteriorDoorId) {
+    public DoorDTO lockExteriorDoor(long exteriorDoorId) {
         // Get the door from the db
-        final Optional<ExteriorDoor>  optionalExteriorDoor = exteriorDoorRepository.findById(exteriorDoorId);
-
-        // Check if door exists
-        if (!optionalExteriorDoor.isPresent()) {
-            throw new ExteriorDoorNotFoundException("Error: Exterior door with ID " + exteriorDoorId + " not found. Please enter a valid exterior door ID.");
-        }
-
-        ExteriorDoor exteriorDoor = optionalExteriorDoor.get();
+        final ExteriorDoor exteriorDoor = exteriorDoorRepository.findById(exteriorDoorId).orElseThrow(() -> new ExteriorDoorNotFoundException(getExteriorDoorNotFoundError(exteriorDoorId)));
 
         // Check if door is already locked
         if (exteriorDoor.isLocked()) {
@@ -124,18 +130,14 @@ public class HouseService {
         // Lock door
         exteriorDoor.setLocked(true);
         exteriorDoorRepository.save(exteriorDoor);
+
+        // Return DoorDTO to client
+        return mvcConversionService.convert(exteriorDoor, DoorDTO.class);
     }
 
-    public void turnOnLight(long lightId) {
+    public LightDTO turnOnLight(long lightId) {
         // Get the light from the db
-        final Optional<Light> optionalLight = lightRepository.findById(lightId);
-
-        // Check if light exists
-        if (!optionalLight.isPresent()) {
-            throw new LightNotFoundException("Error: Light with ID " + lightId + " does not exist. Please enter a valid Light ID.");
-        }
-
-        Light light = optionalLight.get();
+        final Light light = lightRepository.findById(lightId).orElseThrow(() -> new LightNotFoundException(getLightNotFoundError(lightId)));
 
         // Check if light is already on
         if (light.isLightOn()) {
@@ -145,18 +147,14 @@ public class HouseService {
         // Turn on light
         light.setLightOn(true);
         lightRepository.save(light);
+
+        // Return LightDTO to client
+        return mvcConversionService.convert(light, LightDTO.class);
     }
 
-    public void turnOffLight(long lightId) {
+    public LightDTO turnOffLight(long lightId) {
         // Get the light from the db
-        final Optional<Light> optionalLight = lightRepository.findById(lightId);
-
-        // Check if light exists
-        if (!optionalLight.isPresent()) {
-            throw new LightNotFoundException("Error: Light with ID " + lightId + " does not exist. Please enter a valid Light ID.");
-        }
-
-        Light light = optionalLight.get();
+        final Light light = lightRepository.findById(lightId).orElseThrow(() -> new LightNotFoundException(getLightNotFoundError(lightId)));
 
         // Check if light is already off
         if (!light.isLightOn()) {
@@ -166,5 +164,8 @@ public class HouseService {
         // Turn off light
         light.setLightOn(false);
         lightRepository.save(light);
+
+        // Return LightDTO to client
+        return mvcConversionService.convert(light, LightDTO.class);
     }
 }
