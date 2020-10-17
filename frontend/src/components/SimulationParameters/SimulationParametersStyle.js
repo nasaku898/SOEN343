71da5e40cb047d4322e30f982a0ff04ae8445778
Simon Lim <simonlim898@hotmail.com>
2020-10-16 20:28:39 -0400

    #62 Removed simulation paramter component

--- frontend/src/components/SimulationParameters/SimulationParametersStyle.js
+++
@@ -1,8 +0,0 @@
-const { makeStyles } = require("@material-ui/core");
-
-const useStyles = makeStyles((theme) => ({
-    modal:{
-        backgroundColor: theme.palette.background.paper
-    }
-}))
-export default useStyles
