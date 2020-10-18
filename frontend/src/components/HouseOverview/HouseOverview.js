318dc04bfa893aa2b69e8e38a58110442ce08051
Simon Lim <simonlim898@hotmail.com>
2020-10-18 18:28:14 -0400

    #63 Added House overview component representing the layout

---
+++ frontend/src/components/HouseOverview/HouseOverview.js
@@ -0,0 +1,44 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { fetchHouseState } from '../../modules/HouseOverview/HouseOverviewAPI'
+import Paper from '@material-ui/core/Paper';
+import RoomInfo from '../RoomInfo/RoomInfo';
+const HouseOverview = () => {
+
+    const [rows, setRows] = useState([])
+
+    useEffect(() => {
+        fetchHouseState().then(response => {
+            setRows(response)
+        })
+    }, [])
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Room</TableCell>
+                            <TableCell align="right">Temperature</TableCell>
+                            <TableCell align="right">Lights</TableCell>
+                            <TableCell align="right">Doors</TableCell>
+                            <TableCell align="right">Windows</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {
+                            rows.map((row) => (
+                                <RoomInfo key={row.name} row={row}></RoomInfo>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
+
+export default HouseOverview
