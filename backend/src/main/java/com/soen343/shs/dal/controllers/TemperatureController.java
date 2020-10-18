271f6d9ded719fc35936dcf1ec87fffca5cdd896
Simon Lim <simonlim898@hotmail.com>
2020-10-18 18:27:20 -0400

    #63 added new endpoint for setting temperature and fetching rooms

--- backend/src/main/java/com/soen343/shs/dal/controllers/TemperatureController.java
+++ backend/src/main/java/com/soen343/shs/dal/controllers/TemperatureController.java
@@ -1,2 +1,24 @@
-package com.soen343.shs.dal.controllers;public class TemperatureController {
+package com.soen343.shs.dal.controllers;
+
+import com.soen343.shs.dal.service.TemperatureService;
+import com.soen343.shs.dto.RoomDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/simulation")
+public class TemperatureController {
+
+    private final TemperatureService temperatureService;
+
+    public TemperatureController(TemperatureService temperatureService) {
+        this.temperatureService = temperatureService;
+    }
+
+    @PutMapping(value = "/room/{roomId}/temperature/{newTemperature}")
+    public RoomDTO modifyRoomTemperature(@PathVariable final long roomId, @PathVariable final double newTemperature) {
+        return temperatureService.setTemperatureOfRoom(roomId, newTemperature);
+    }
 }
