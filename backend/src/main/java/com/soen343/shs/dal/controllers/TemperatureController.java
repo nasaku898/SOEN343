a3daa6d46d12dc2f6db446aeba10dba17e07579d
Simon Lim <simonlim898@hotmail.com>
2020-10-19 14:16:24 -0400

    #63 Added lombok notation

--- backend/src/main/java/com/soen343/shs/dal/controllers/TemperatureController.java
+++ backend/src/main/java/com/soen343/shs/dal/controllers/TemperatureController.java
@@ -2,6 +2,7 @@ package com.soen343.shs.dal.controllers;
 
 import com.soen343.shs.dal.service.TemperatureService;
 import com.soen343.shs.dto.RoomDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/simulation")
+@RequiredArgsConstructor
 public class TemperatureController {
 
     private final TemperatureService temperatureService;
-
-    public TemperatureController(TemperatureService temperatureService) {
-        this.temperatureService = temperatureService;
-    }
 
     @PutMapping(value = "/room/{roomId}/temperature/{newTemperature}")
     public RoomDTO modifyRoomTemperature(@PathVariable final long roomId, @PathVariable final double newTemperature) {
