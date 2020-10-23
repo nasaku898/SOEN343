62773ab9de89a762db12785219326f0edeb395c2
Josh Fried <josh.fried13@gmail.com>
2020-10-23 00:01:21 -0400

    Added mapper

--- backend/src/main/java/com/soen343/shs/dal/repository/mapping/SHSUserMapper.java
+++ backend/src/main/java/com/soen343/shs/dal/repository/mapping/SHSUserMapper.java
@@ -1,2 +1,16 @@
-package com.soen343.shs.dal.repository.mapping;public interface SHSUserMapper {
+package com.soen343.shs.dal.repository.mapping;
+
+import com.soen343.shs.dal.model.User;
+import com.soen343.shs.dto.UserDTO;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface SHSUserMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    User updateUserFromDTO(UserDTO dto, @MappingTarget User user);
+
+
 }
